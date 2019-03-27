38cc26e1551358052fa0f4f3d88b861d14cf4ade
Dima <kovenev1998@gmail.com>
2019-03-27 14:43:31 +0200

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+var message = 'Hello' 	 // 2 задание, текстовая переменная
+console.log(message);  
+
+var myNumber = 5; 			 // 2 задание, числовая переменная
+console.log(myNumber);  
+
+console.log('1' > '2');  // 2 задание, логическая переменная
+
+var obj = {};						 // 2 задание, объект
+obj.text = "Hi";         
+console.log(obj);
+
+var array = [1, 2, 3, 4, 5];  // 2 задание, массив
+array[2] = 7;
+console.log(array);
+
+
+console.log('');  
+
+
+var n = 3.1415926;						// 3 задание
+console.log(n.toFixed(2));  
+
+
+console.log('');  
+
+
+var i = 5; 					 //задание 4
+console.log(--i);        
+console.log(i);
+console.log(i++);
+console.log(i);
+
+
+console.log('');  
+
+
+var n = 10;          // 5 задание                
+n += 5;
+console.log(n);
+n -= 2; 
+console.log(n);
+n *= 2;            
+console.log(n);
+n /= 2;
+console.log(n);
+n %= 4;
+console.log(n);
+
+
+console.log('');  
+
+
+console.log(Math.PI);                // 6 задание
+console.log(Math.round(89.279));
+console.log(Math.random()*10);
+console.log(Math.pow(2,5));
+
+
+console.log('');  
+
+var string = 'Lorem ipsum dolor sit';     // 7 задание
+console.log(string.length);  
+
+
+console.log('');  
+
+
+var string = 'Lorem ipsum dolor sit amet';  // 8 задание
+console.log(string.indexOf('ips'));  
+console.log(string.indexOf('not')); 
+
+
+console.log('');  
+
+
+var string = 'Lorem ipsum dolor sit amet';       // 9 задание
+console.log(string.replace('sit', 'not sit'));
+
+
+console.log('');  
+
+
+var myString = 'Lorem Ipsum Dolor Sit.';   // 10 задание
+console.log(myString);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+
+
